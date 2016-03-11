e5a05151d03d01663a783e73ba88e463216d8024
babywolf <591138406@qq.com>
2016-03-11 14:55:18 +0800

    init project

---
+++ ShadeNavigationController/ShadeNavigationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShadeNavigationController
+//
+//  Created by babywolf on 16/3/11.
+//  Copyright © 2016年 com.babywolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ShadeNavigationController/ShadeNavigationController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShadeNavigationController
+//
+//  Created by babywolf on 16/3/11.
+//  Copyright © 2016年 com.babywolf. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
